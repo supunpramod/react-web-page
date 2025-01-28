5cd3be318b49640d4efc5817c8c8f3254adff4d8
Supun Pramod <pramodsupun06@gmail.com>
2025-01-28 19:21:42 +0530

    my react web sites

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      <h1>My first React Web page</h1>
+      <button className="users-button">Users</button>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
